aad40b01ba28a47de777f1f79c8714a7848afade
Your Name <dpacoria@unsa.edu.pe>
2024-04-27 02:11:46 -0500

    Se configuro eslint y tailwindCSS para agregar colores

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+            colors: {
+                'primary': {
+                    '50': '#f0f7fe',
+                    '100': '#dcedfd',
+                    '200': '#c1e1fc',
+                    '300': '#97cff9',
+                    '400': '#65b5f5',
+                    '500': '#4799f0',
+                    '600': '#2c79e4',
+                    '700': '#2363d2',
+                    '800': '#2351aa',
+                    '900': '#224786',
+                    '950': '#192c52',
+                },
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
